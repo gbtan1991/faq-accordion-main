278d13f5755c74ea95aed25e1ac384cdc1e61607
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2023-12-20 17:03:51 +0800

    working with the animation and closing function

--- src/component/Accordion.jsx
+++ src/component/Accordion.jsx
@@ -1,33 +1,34 @@
 import React, { useState } from "react";
-import plusIcon from '../assets/images/icon-plus.svg'
-import minusIcon from '../assets/images/icon-minus.svg'
+import plusIcon from "../assets/images/icon-plus.svg";
+import minusIcon from "../assets/images/icon-minus.svg";
 
-const Accordion = ({ faq, isLastItem }) => {
-    const [isOpen, setIsOpen] = useState(false)
+const Accordion = ({ faq, isLastItem, index }) => {
+  const [openIndex, setOpenIndex] = useState(-1);
 
-    const handleOpenClick = () =>{
-        
-        setIsOpen(!isOpen)
-    }
+  const handleOpenClick = (index) => {
+    setOpenIndex(index === openIndex ? -1 : index);
+  };
 
-
-
-    const borderStyling = `${isLastItem ? ' border-b-0' : 'border-b pb-4' }`;
+  const borderStyling = `${isLastItem ? " border-b-0" : "border-b pb-4"}`;
   return (
     <div className={borderStyling}>
-    <div className="flex flex-row justify-between items-center space-x-5 py-4 px-2">
-      <div>
-        <h3 className="text-base/none text-dark-purple font-bold ">{faq.question}</h3>
+      <div className="flex flex-row justify-between items-center space-x-5 py-4 px-2">
+        <div>
+          <h3 className="text-base/none text-dark-purple font-bold ">
+            {faq.question}
+          </h3>
+        </div>
+        <div>
+          <button className="w-[30px] h-full" onClick={() => handleOpenClick(index)}>
+            <img
+              src={openIndex === index ? minusIcon : plusIcon}
+              alt={openIndex === index ? "minus icon" : "plus icon"}
+              className=""
+            />
+          </button>
+        </div>
       </div>
-      <div>
-      <button className="w-[30px] h-full" onClick={handleOpenClick}>
-        <img src={isOpen ? minusIcon : plusIcon} alt={isOpen ? "minus icon" : "plus icon"} className="" />
-      </button>
-    
-      </div>
-      
-    </div>
-    {isOpen && (
+      {openIndex === index && (
         <div className="mt-2 px-2">
           <p className="text-sm">{faq.answer}</p>
         </div>
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -3,18 +3,10 @@ import starIcon from "../assets/images/icon-star.svg";
 import Accordion from "./Accordion";
 
 const Card = ({ faqList }) => {
-  const [openAccordion, setOpenAccordion] = useState(null);
 
-  const closeOthers = () => {
-    setOpenAccordion(null);
-  };
-
-  const handleAccordionClick = (index) => {
-    setOpenAccordion(index);
-  };
 
   return (
-    <div className="absolute z-10 top-24 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-5 bg-white rounded-lg w-[325px] flex flex-col gap-2">
+    <div className="absolute z-10 top-24 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-5 bg-white rounded-lg w-[325px] sm:w-[600px]  flex flex-col gap-2">
       <div className="flex flex-row gap-5">
         <img src={starIcon} alt="star icon" className="w-7" />
         <h1 className="text-3xl text-dark-purple font-theme font-bold">FAQs</h1>
@@ -26,6 +18,7 @@ const Card = ({ faqList }) => {
               faq={faq}
               key={index}
               isLastItem={index === faqList.length - 1}
+              
             />
           </div>
         ))}
