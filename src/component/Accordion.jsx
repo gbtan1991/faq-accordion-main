4371f0fdf18e418dc9128c68965a05ee2056189d
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2023-12-21 13:44:35 +0800

    Add the final touches before uploading/publishing

--- src/component/Accordion.jsx
+++ src/component/Accordion.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import classNames from "classnames";
 import plusIcon from "../assets/images/icon-plus.svg";
 import minusIcon from "../assets/images/icon-minus.svg";
 
@@ -8,10 +9,18 @@ const Accordion = ({ faq, isLastItem, index }) => {
   const handleOpenClick = (index) => {
     setOpenIndex(index === openIndex ? -1 : index);
   };
+  const containerClasses = classNames({
+    "border-b": !isLastItem,
+    "pb-4": !isLastItem,
+  });
 
-  const borderStyling = `${isLastItem ? " border-b-0" : "border-b pb-4"}`;
+  const contentClasses = classNames({
+    "mt-2": true,
+    "px-2": true,
+    "fade-in": openIndex === index,
+  });
   return (
-    <div className={borderStyling}>
+    <div className={containerClasses}>
       <div className="flex flex-row justify-between items-center space-x-5 py-4 px-2">
         <div>
           <h3 className="text-base/none text-dark-purple font-bold ">
@@ -19,7 +28,10 @@ const Accordion = ({ faq, isLastItem, index }) => {
           </h3>
         </div>
         <div>
-          <button className="w-[30px] h-full" onClick={() => handleOpenClick(index)}>
+          <button
+            className="w-[30px] h-full"
+            onClick={() => handleOpenClick(index)}
+          >
             <img
               src={openIndex === index ? minusIcon : plusIcon}
               alt={openIndex === index ? "minus icon" : "plus icon"}
@@ -29,7 +41,7 @@ const Accordion = ({ faq, isLastItem, index }) => {
         </div>
       </div>
       {openIndex === index && (
-        <div className="mt-2 px-2">
+        <div className={contentClasses}>
           <p className="text-sm">{faq.answer}</p>
         </div>
       )}
